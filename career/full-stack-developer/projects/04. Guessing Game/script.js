80e647c40972b6b59453e074fa68e68925cd4935
Kener de Oliveira Branco <115368851+kenerbranco@users.noreply.github.com>
2025-04-22 01:04:39 -0300

    Update script.js

--- career/full-stack-developer/projects/04. Guessing Game/script.js
+++ career/full-stack-developer/projects/04. Guessing Game/script.js
@@ -1,4 +1,5 @@
 let countChance = 0;
+let winGame = false;
 const randomNumber = Math.floor(Math.random() * 101);
 const answerDisplay = document.getElementById("answerDisplay");
 
@@ -7,17 +8,22 @@ function checkNumber() {
   const numberInputStr = document.getElementById("inputNumber").value;
   const numberInputNum = Number(numberInputStr);
 
-  if (countChance <= 10) {
+  if (countChance <= 10 && winGame === false) {
     if (numberInputNum === randomNumber) {
       answerDisplay.innerHTML = "The number is " + randomNumber + ". You WIN!!. (Attempts: " + countChance + " of 10)";
-    } else if (numberInputNum < randomNumber) {
+      winGame = true;
+    } else if (numberInputNum < randomNumber && winGame === false) {
       answerDisplay.innerHTML = "Secret number is higher than " + numberInputNum + ". (Attempts: " + countChance + " of 10)";
 
-    } else if (numberInputNum > randomNumber) {
+    } else if (numberInputNum > randomNumber && winGame === false) {
       answerDisplay.innerHTML = "Secret number is lower than " + numberInputNum + ". (Attempts: " + countChance + " of 10)";
     }
 
-  } else {
+  } else if (winGame === true) {
+    answerDisplay.innerHTML = "You WON. Restart the game to play again!";
+  }
+  
+  else {
     answerDisplay.innerHTML = "You have reached the limit of attempts. Restart the game!";
   }
 }
