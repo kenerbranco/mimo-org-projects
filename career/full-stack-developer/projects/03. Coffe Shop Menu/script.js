6f86037f8b9186687fb954df3d6fd59bc8f0fff6
Kener de Oliveira Branco <115368851+kenerbranco@users.noreply.github.com>
2025-04-21 23:50:23 -0300

    Create script.js

---
+++ career/full-stack-developer/projects/03. Coffe Shop Menu/script.js
@@ -0,0 +1,29 @@
+function choiceItemOne() {
+  const img = document.querySelector("img");
+  img.setAttribute("src", "https://i.postimg.cc/ZWcKjdBb/5376.jpg");
+}
+
+function choiceItemTwo() {
+  const img = document.querySelector("img");
+  img.setAttribute("src", "https://i.postimg.cc/MMP8j7F4/2825.jpg");
+}
+
+function choiceItemThree() {
+  const img = document.querySelector("img");
+  img.setAttribute("src", "https://i.postimg.cc/s1Rqf0HL/4603.jpg");
+}
+
+function choiceItemFour() {
+  const img = document.querySelector("img");
+  img.setAttribute("src", "https://i.postimg.cc/XBwCQv6w/10225.jpg");
+}
+
+function choiceItemFive() {
+  const img = document.querySelector("img");
+  img.setAttribute("src", "https://i.postimg.cc/DWhg1tVJ/2150777669.jpg");
+}
+
+function choiceItemSix() {
+  const img = document.querySelector("img");
+  img.setAttribute("src", "https://i.postimg.cc/6TBPtyYZ/2148296825.jpg");
+}
