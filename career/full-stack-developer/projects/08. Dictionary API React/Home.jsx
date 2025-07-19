85211e2c181beaca7b4264c6376d67a5d606a92f
Kener de Oliveira Branco <115368851+kenerbranco@users.noreply.github.com>
2025-07-19 05:53:05 -0300

    Create Home.jsx

---
+++ career/full-stack-developer/projects/08. Dictionary API React/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1>Welcome to the Online Dictionary!</h1>
+      <p>This is your portal to explore the meaning of words in English. Use the Search page to find definitions, parts of speech, and examples.</p>
+      <p>We hope you enjoy it!</p>
+    </div>
+  );
+};
+
+export default Home;
